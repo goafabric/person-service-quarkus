5d2f61f5ec98a7eda306676006386b66d2583f8e
Andreas Mautsch <amautsch>
2021-06-05 17:59:15 +0200

    persistence added

---
+++ src/main/java/org/goafabric/personservice/persistence/DemoDataInitializer.java
@@ -0,0 +1,27 @@
+package org.goafabric.personservice.persistence;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class DemoDataInitializer {
+
+    @Inject
+    private PersonRepository personRepository;
+
+    public void run() {
+        if (!personRepository.findAll().iterator().hasNext()) {
+            personRepository.save(PersonBo.builder()
+                    .firstName("Homer").lastName("Simpson")
+                    .build());
+
+            personRepository.save(PersonBo.builder()
+                    .firstName("Bart").lastName("Simpson")
+                    .build());
+
+            personRepository.save(PersonBo.builder()
+                    .firstName("Monty").lastName("Burns")
+                    .build());
+        }
+    }
+}
