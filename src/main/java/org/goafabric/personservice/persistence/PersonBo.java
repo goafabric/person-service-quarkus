5a6cc26bba14246b546b2d6eae25221e73d03c17
Andreas Mautsch <amautsch>
2021-06-06 11:54:00 +0200

    flyway added

--- src/main/java/org/goafabric/personservice/persistence/PersonBo.java
+++ src/main/java/org/goafabric/personservice/persistence/PersonBo.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Table;
@@ -19,7 +20,9 @@ public class PersonBo {
     @javax.persistence.Id @GeneratedValue(generator = "uuid") @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
 }
