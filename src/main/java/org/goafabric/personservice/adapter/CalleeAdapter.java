c2912fc4319914b33f767c7423a0fd45f3ef5150
Andreas Mautsch <amautsch>
2021-06-14 16:29:21 +0200

    CalleeAdapter added

--- src/main/java/org/goafabric/personservice/adapter/CalleeAdapter.java
+++ src/main/java/org/goafabric/personservice/adapter/CalleeAdapter.java
@@ -2,11 +2,13 @@ package org.goafabric.personservice.adapter;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/callees")
 @RegisterRestClient
+@Singleton
 public interface CalleeAdapter {
 
     @GET
