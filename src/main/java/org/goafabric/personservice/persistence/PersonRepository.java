bdf6e1371680ec91f919a26c9a25c8964e944330
Andreas Mautsch <amautsch>
2021-07-21 10:13:12 +0200

    repo impl

--- src/main/java/org/goafabric/personservice/persistence/PersonRepository.java
+++ src/main/java/org/goafabric/personservice/persistence/PersonRepository.java
@@ -8,11 +8,11 @@ import java.util.List;
 @ApplicationScoped
 public class PersonRepository implements PanacheRepositoryBase<PersonBo, String> {
     public List<PersonBo> findByFirstName(String firstName) {
-        return find("firstName", firstName).list();
+        return find("firstName = ?1 and tenantId = ?2", firstName, "0").list();
     }
 
     public List<PersonBo> findByLastName(String lastName) {
-        return find("lastName", lastName).list();
+        return find("lastName = ?1 and tenantId = ?2", lastName, "0").list();
     }
 
     public PersonBo save(PersonBo personBo) {
