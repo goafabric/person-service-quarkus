ae9e1975b99ef90617fbd0b2a49e139ab122b384
Andreas Mautsch <amautsch>
2021-07-23 14:19:26 +0200

    Changes by daniel_vivasestevao

--- src/main/java/org/goafabric/personservice/persistence/multitenancy/RepositoryDelegate.java
+++ src/main/java/org/goafabric/personservice/persistence/multitenancy/RepositoryDelegate.java
@@ -5,5 +5,5 @@ import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class RepositoryDelegate<T, I> implements PanacheRepositoryBase<T, I> {
+public class RepositoryDelegate<T extends TenantAware, I> implements PanacheRepositoryBase<T, I> {
 }
