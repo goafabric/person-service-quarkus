70e0a35b7f08a627b1043adaac037069a04d9b9b
Andreas Mautsch <amautsch>
2021-09-14 16:19:34 +0200

    multi tenancy added

--- src/main/java/org/goafabric/personservice/crossfunctional/TenantIdResolver.java
+++ src/main/java/org/goafabric/personservice/crossfunctional/TenantIdResolver.java
@@ -1,17 +1,38 @@
 package org.goafabric.personservice.crossfunctional;
 
+import io.quarkus.oidc.OidcTenantConfig;
+import io.quarkus.oidc.TenantConfigResolver;
+import io.smallrye.mutiny.Uni;
 import io.vertx.ext.web.RoutingContext;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.ConfigProvider;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 @Slf4j
-public class TenantIdResolver implements io.quarkus.oidc.TenantResolver {
+public class TenantIdResolver implements TenantConfigResolver {
 
     @Override
-    public String resolve(RoutingContext context) {
-        log.info("#Tenantresolver got: " + context.request().getHeader("X-TenantId"));
-        return "tenant-" + context.request().getHeader("X-TenantId");
+    public Uni<OidcTenantConfig> resolve(RoutingContext routingContext, TenantConfigResolver.TenantConfigRequestContext requestContext) {
+        String tenantId = routingContext.request().getHeader("X-TenantId");
+        log.info("#Tenantresolver got: " + tenantId);
+
+        if (tenantId == null) {
+            tenantId = "0";
+        }
+
+        final OidcTenantConfig config = new OidcTenantConfig();
+        //config.setApplicationType(ConfigProvider.getConfig().getValue("quarkus.oidc.application-type", OidcTenantConfig.ApplicationType.class));
+        config.setApplicationType(OidcTenantConfig.ApplicationType.HYBRID);
+
+        //config.setRoles(OidcTenantConfig.Roles.Source.accesstoken);
+        config.setClientId(ConfigProvider.getConfig().getValue("quarkus.oidc.client-id", String.class));
+
+        config.setTenantId("tenant-" + tenantId);
+        config.setAuthServerUrl(
+                ConfigProvider.getConfig().getValue("quarkus.oidc.auth-server-url", String.class) + "/" + config.getTenantId().get());
+
+        return Uni.createFrom().item(config);
     }
 }
