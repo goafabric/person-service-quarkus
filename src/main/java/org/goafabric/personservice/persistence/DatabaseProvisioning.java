c17a37d72ae4177f4e15b8402cbe480642323fad
Andreas Mautsch <amautsch>
2022-01-16 13:23:33 +0100

    audit fixes

---
+++ src/main/java/org/goafabric/personservice/persistence/DatabaseProvisioning.java
@@ -0,0 +1,48 @@
+package org.goafabric.personservice.persistence;
+
+import io.quarkus.runtime.Quarkus;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+@Transactional
+@Slf4j
+public class DatabaseProvisioning {
+    @ConfigProperty(name = "database.provisioning.goals", defaultValue = " ")
+    String goals;
+
+    @Inject
+    PersonRepository personRepository;
+
+    public void run() {
+        if (goals.contains("-import-demo-data")) {
+            log.info("Importing demo data ...");
+            importDemoData();
+        }
+
+        if (goals.contains("-terminate")) {
+            log.info("Terminating app ...");
+            Quarkus.asyncExit();
+        }
+    }
+
+    public void importDemoData() {
+        if (personRepository.findAll().list().isEmpty()) {
+            personRepository.save(PersonBo.builder()
+                    .firstName("Homer").lastName("Simpson").secret("SuperSecret")
+                    .build());
+
+            personRepository.save(PersonBo.builder()
+                    .firstName("Bart").lastName("Simpson").secret("SuperSecret")
+                    .build());
+
+            personRepository.save(PersonBo.builder()
+                    .firstName("Monty").lastName("Burns").secret("SuperSecret")
+                    .build());
+        }
+    }
+}
