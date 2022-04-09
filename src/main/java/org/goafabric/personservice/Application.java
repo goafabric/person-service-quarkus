887e4216293dc1c85dd72ef8f97fa1ff73c36fb5
Andreas Mautsch <amautsch>
2022-04-09 13:08:27 +0200

    easier security

--- src/main/java/org/goafabric/personservice/Application.java
+++ src/main/java/org/goafabric/personservice/Application.java
@@ -3,10 +3,17 @@ package org.goafabric.personservice;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import io.quarkus.security.spi.runtime.AuthorizationController;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.goafabric.personservice.persistence.DatabaseProvisioning;
 
+import javax.annotation.Priority;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Alternative;
 import javax.inject.Inject;
+import javax.interceptor.Interceptor;
 
 @QuarkusMain
 @Slf4j
@@ -17,9 +24,7 @@ public class Application {
     }
 
     public static class MyApp implements QuarkusApplication {
-
-        @Inject
-        DatabaseProvisioning databaseProvisioning;
+        @Inject DatabaseProvisioning databaseProvisioning;
 
         @Override
         public int run(String... args) throws Exception {
@@ -27,6 +32,15 @@ public class Application {
             Quarkus.waitForExit();
             return 0;
         }
+    }
+
+    @Getter
+    @Alternative
+    @ApplicationScoped
+    @Priority(Interceptor.Priority.LIBRARY_AFTER)
+    static class SecurityConfiguration extends AuthorizationController {
+        @ConfigProperty(name = "security.authentication.enabled", defaultValue = "true")
+        boolean isAuthorizationEnabled;
     }
 
 }
