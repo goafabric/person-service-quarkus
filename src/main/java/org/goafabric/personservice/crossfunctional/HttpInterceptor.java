25a5a1be7e19abe696a06c9a6d85cf334e933aed
Andreas Mautsch <amautsch>
2023-02-27 10:24:03 +0100

    changes

--- src/main/java/org/goafabric/personservice/crossfunctional/HttpInterceptor.java
+++ src/main/java/org/goafabric/personservice/crossfunctional/HttpInterceptor.java
@@ -18,13 +18,9 @@ public class HttpInterceptor implements ContainerRequestFilter, ContainerRespons
     private static final ThreadLocal<String> tenantId = new ThreadLocal<>();
     private static final ThreadLocal<String> userName = new ThreadLocal<>();
 
-    public static String getTenantId() { return tenantId.get(); }
-    public static String getUserName() { return userName.get(); }
-    public static void   setTenantId(String tenantId) { HttpInterceptor.tenantId.set(tenantId); }
-
     @Override
     public void filter(ContainerRequestContext request) throws IOException {
-        tenantId.set(request.getHeaderString("X-TenantId") != null ? request.getHeaderString("X-TenantId") : "0"); //TODO
+        tenantId.set(request.getHeaderString(request.getHeaderString("X-TenantId")));
         userName.set(request.getHeaderString("X-Auth-Request-Preferred-Username") != null ? request.getHeaderString("X-Auth-Request-Preferred-Username")
                 :  securityIdentity.getPrincipal().getName());
     }
@@ -33,6 +29,18 @@ public class HttpInterceptor implements ContainerRequestFilter, ContainerRespons
     public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
         tenantId.remove();
         userName.remove();
+    }
+
+    public static void setTenantId(String tenantId) {
+        HttpInterceptor.tenantId.set(tenantId);
+    }
+
+    public static String getTenantId() {
+        return tenantId.get() != null ? tenantId.get() : "0"; //tdo
+    }
+
+    public static String getUserName() {
+        return userName.get();
     }
 
 }
