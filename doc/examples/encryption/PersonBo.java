becc6eeae4ed126a4d8ffdaaf0276c28f63f59a6
Andreas Mautsch <amautsch>
2023-04-06 10:09:27 +0200

    examples added

--- doc/examples/encryption/PersonBo.java
+++ doc/examples/encryption/PersonBo.java
@@ -1,28 +1,30 @@
 package org.goafabric.personservice.persistence.domain;
 
+import jakarta.persistence.*;
 import org.goafabric.personservice.persistence.encryption.StringEncryptor;
 import org.goafabric.personservice.persistence.multitenancy.TenantAware;
 import org.hibernate.annotations.GenericGenerator;
-
-import jakarta.persistence.*;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "person")
+@Where(clause = TenantAware.TENANT_FILTER)
 public class PersonBo extends TenantAware {
     @Id
     @GeneratedValue(generator = "uuid") @GenericGenerator(name = "uuid", strategy = "uuid2")
     public String id;
 
-    @Column(name = "first_name")
-    @Convert(converter = StringEncryptor.class)
+    @Convert(converter = StringEncryptor.class) // be aware that colum size needs to be increased to hold encrypted value
     public String firstName;
 
-    @Column(name = "last_name")
     public String lastName;
 
     @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "address_id", referencedColumnName = "id")
     public AddressBo address;
+
+    @Version //optimistic locking
+    public Long version;
 
     @Override
     public String getId() {
