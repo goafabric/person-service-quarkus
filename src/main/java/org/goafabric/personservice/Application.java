9211f3b117202262f6940ea409651f35a5c4f5ea
Andreas Mautsch <amautsch>
2023-04-17 11:58:57 +0200

    changes

--- src/main/java/org/goafabric/personservice/Application.java
+++ src/main/java/org/goafabric/personservice/Application.java
@@ -1,7 +1,6 @@
 package org.goafabric.personservice;
 
 import io.quarkus.runtime.Quarkus;
-import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import io.quarkus.security.spi.runtime.AuthorizationController;
 import jakarta.annotation.Priority;
@@ -14,36 +13,7 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 public class Application {
 
     public static void main(String... args) {
-        Quarkus.run(MyApp.class, args);
-    }
-
-    public static class MyApp implements QuarkusApplication {
-        @Override
-        public int run(String... args) throws Exception {
-            //doFlyway();
-            Quarkus.waitForExit();
-            return 0;
-        }
-
-        /*
-        public static void doFlyway() {
-            final Flyway flyway = CDI.current().select(Flyway.class).get();
-            final String schemas = ConfigProvider.getConfig().getValue("quarkus.flyway.schemas", String.class);
-            //String schemas = "tenant_0,tenant_5a2f";
-            String schema_prefix = "";
-            Arrays.asList(schemas.split(",")).forEach(schema -> {
-                        Flyway.configure()
-                                .configuration(flyway.getConfiguration())
-                                .schemas(schema_prefix + schema)
-                                .defaultSchema(schema_prefix + schema)
-                                .placeholders(Map.of("tenantId", schema))
-                                .load()
-                                .migrate();
-                    }
-            );
-        }
-
-         */
+        Quarkus.run(args);
     }
 
     @Alternative
--- src/main/java/org/goafabric/personservice/persistence/multitenancy/CustomTenantResolver.java
+++ src/main/java/org/goafabric/personservice/persistence/multitenancy/CustomTenantResolver.java
@@ -2,9 +2,16 @@ package org.goafabric.personservice.persistence.multitenancy;
 
 import io.quarkus.hibernate.orm.PersistenceUnitExtension;
 import io.quarkus.hibernate.orm.runtime.tenant.TenantResolver;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.spi.CDI;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.flywaydb.core.Flyway;
 import org.goafabric.personservice.crossfunctional.HttpInterceptor;
+
+import java.util.Arrays;
+import java.util.Map;
 
 @PersistenceUnitExtension
 @RequestScoped
@@ -20,5 +27,25 @@ public class CustomTenantResolver implements TenantResolver {
     public String resolveTenantId() {
         return "tenant_" + HttpInterceptor.getTenantId();
     }
+
+    @ApplicationScoped
+    static class FlywayConfig {
+        public FlywayConfig() {
+            final Flyway flyway = CDI.current().select(Flyway.class).get();
+            final String schemas = ConfigProvider.getConfig().getValue("quarkus.flyway.schemas", String.class);
+            String schema_prefix = "";
+            Arrays.asList(schemas.split(",")).forEach(schema -> {
+                        Flyway.configure()
+                                .configuration(flyway.getConfiguration())
+                                .schemas(schema_prefix + schema)
+                                .defaultSchema(schema_prefix + schema)
+                                .placeholders(Map.of("tenantId", schema))
+                                .load()
+                                .migrate();
+                    }
+            );
+        }
+    }
+
 
 }
