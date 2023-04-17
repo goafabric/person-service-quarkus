8b2a1cc2600e26f3ec3132c5abb7cb9cf69f1658
Andreas Mautsch <amautsch>
2023-04-17 11:56:13 +0200

    changes

--- src/main/java/org/goafabric/personservice/persistence/multitenancy/CustomTenantResolver.java
+++ src/main/java/org/goafabric/personservice/persistence/multitenancy/CustomTenantResolver.java
@@ -3,11 +3,13 @@ package org.goafabric.personservice.persistence.multitenancy;
 import io.quarkus.hibernate.orm.PersistenceUnitExtension;
 import io.quarkus.hibernate.orm.runtime.tenant.TenantResolver;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 import org.goafabric.personservice.crossfunctional.HttpInterceptor;
 
 @PersistenceUnitExtension
 @RequestScoped
 public class CustomTenantResolver implements TenantResolver {
+    @Inject FlywayConfig flywayConfig;
 
     @Override
     public String getDefaultTenantId() {
