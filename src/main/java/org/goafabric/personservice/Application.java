6b8b2b9135a2af8cd835c57ee2f56904ef878969
Andreas Mautsch <amautsch>
2024-10-24 10:01:50 +0200

    jakarta data

--- src/main/java/org/goafabric/personservice/Application.java
+++ src/main/java/org/goafabric/personservice/Application.java
@@ -2,12 +2,6 @@ package org.goafabric.personservice;
 
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
-import io.quarkus.security.spi.runtime.AuthorizationController;
-import jakarta.annotation.Priority;
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Alternative;
-import jakarta.interceptor.Interceptor;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @QuarkusMain
 public class Application {
@@ -15,14 +9,5 @@ public class Application {
     public static void main(String... args) {
         Quarkus.run(args);
     }
-
-    @Alternative
-    @ApplicationScoped @Priority(Interceptor.Priority.LIBRARY_AFTER)
-    static class SecurityConfiguration extends AuthorizationController {
-        @ConfigProperty(name = "security.authentication.enabled", defaultValue = "true")
-        boolean isAuthorizationEnabled;
-        public boolean isAuthorizationEnabled() { return isAuthorizationEnabled; }
-    }
-    
 
 }
