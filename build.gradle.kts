f3d2b151958a357b6a0edcc3a62bac29de98e17a
Andreas Mautsch <amautsch@gmx.de>
2025-09-03 12:42:41 +0000

    Update all-updates to v3.26.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ val dockerRegistry = "goafabric"
 plugins {
 	java
 	jacoco
-	id("io.quarkus") version "3.26.1"
+	id("io.quarkus") version "3.26.2"
 	id("net.researchgate.release") version "3.1.0"
 }
 
@@ -22,7 +22,7 @@ dependencies {
 		testImplementation("org.assertj:assertj-core:3.27.4")
 	}
 
-	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.26.1"))
+	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.26.2"))
 }
 dependencies {
 	//web
