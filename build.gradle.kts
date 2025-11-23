e534a8012e9482414a4762d32f5f2521a8f92b27
Andreas Mautsch <amautsch@gmx.de>
2025-11-23 01:59:23 +0000

    Update dependency org.hibernate.orm:hibernate-jpamodelgen to v7.1.10.Final

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
 	//jakarta data
 	implementation("io.quarkus:quarkus-hibernate-orm")
 	implementation("jakarta.data:jakarta.data-api:1.0.1")
-	annotationProcessor("org.hibernate.orm:hibernate-jpamodelgen:7.1.9.Final")
+	annotationProcessor("org.hibernate.orm:hibernate-jpamodelgen:7.1.10.Final")
 
 	//adapter
 	implementation("io.quarkus:quarkus-resteasy-client-jackson")
