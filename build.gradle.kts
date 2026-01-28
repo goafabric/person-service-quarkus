dd134426ef9a761f8e9346a8e8e7bccf8d11e917
Andreas Mautsch <amautsch@gmx.de>
2026-01-28 13:07:50 +0000

    Update all-updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ val dockerRegistry = "goafabric"
 plugins {
 	java
 	jacoco
-	id("io.quarkus") version "3.30.6"
+	id("io.quarkus") version "3.31.1"
 	id("net.researchgate.release") version "3.1.0"
 }
 
@@ -19,10 +19,10 @@ dependencies {
 	constraints {
 		implementation("org.mapstruct:mapstruct:1.6.3")
 		annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
-		testImplementation("org.assertj:assertj-core:3.27.6")
+		testImplementation("org.assertj:assertj-core:3.27.7")
 	}
 
-	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.30.6"))
+	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.31.1"))
 }
 dependencies {
 	//web
@@ -52,7 +52,7 @@ dependencies {
 	//jakarta data
 	implementation("io.quarkus:quarkus-hibernate-orm")
 	implementation("jakarta.data:jakarta.data-api:1.0.1")
-	annotationProcessor("org.hibernate.orm:hibernate-jpamodelgen:7.2.1.Final")
+	annotationProcessor("org.hibernate.orm:hibernate-jpamodelgen:7.2.2.Final")
 
 	//adapter
 	implementation("io.quarkus:quarkus-resteasy-client-jackson")
