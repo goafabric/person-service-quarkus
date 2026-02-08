670aafa8a7310a0f920d2214d0e1ee336cfcb84e
Andreas Mautsch <amautsch>
2026-02-08 11:12:32 +0100

    kotlin

--- src/main/kotlin/org/goafabric/personservice/Application.kt
+++ src/main/kotlin/org/goafabric/personservice/Application.kt
@@ -4,9 +4,8 @@ import io.quarkus.runtime.Quarkus
 import io.quarkus.runtime.annotations.QuarkusMain
 
 @QuarkusMain
-object Application {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        Quarkus.run(*args)
-    }
+class Application
+
+fun main(args: Array<String>) {
+    Quarkus.run(*args)
 }
