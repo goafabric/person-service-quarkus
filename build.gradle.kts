947839d9b2fa3752f40af14e7f25bdae25319a8a
Andreas Mautsch <amautsch>
2026-02-10 11:31:37 +0100

    chang

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,7 +105,7 @@ tasks.register<Exec>("dockerImageNative") { group = "build" ; dependsOn("quarkus
 		System.setProperty("quarkus.native.container-build", "true")
 		System.setProperty("quarkus.container-image.build", "true")
 
-		System.setProperty("quarkus.native.native-image-xmx", "4096m")
+		System.setProperty("quarkus.native.native-image-xmx", "5000m")
 		//System.setProperty("quarkus.jib.base-native-image", "registry.access.redhat.com/ubi8/ubi-minimal:8.10")
 		System.setProperty("quarkus.container-image.image", "${dockerRegistry}/${project.name}:${project.version}")
 
