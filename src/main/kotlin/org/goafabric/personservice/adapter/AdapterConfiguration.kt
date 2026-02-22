8ee47a889b1fbcb50735d3968ad85afe039d2c92
Andreas Mautsch <amautsch>
2026-02-22 09:34:26 +0100

    chang

--- src/main/kotlin/org/goafabric/personservice/adapter/AdapterConfiguration.kt
+++ src/main/kotlin/org/goafabric/personservice/adapter/AdapterConfiguration.kt
@@ -2,17 +2,17 @@ package org.goafabric.personservice.adapter
 
 import jakarta.ws.rs.core.MultivaluedMap
 import org.eclipse.microprofile.rest.client.ext.ClientHeadersFactory
-import org.goafabric.personservice.extensions.UserContext.adapterHeaderMap
+import org.goafabric.personservice.extensions.UserContext
 import org.jboss.resteasy.specimpl.MultivaluedMapImpl
 
 class AdapterConfiguration : ClientHeadersFactory {
 
     override fun update(
-        multivaluedMap: MultivaluedMap<String?, String?>?,
-        multivaluedMap1: MultivaluedMap<String?, String?>?
-    ): MultivaluedMap<String?, String?> {
-        val result: MultivaluedMap<String?, String?> = MultivaluedMapImpl<String?, String?>()
-        adapterHeaderMap.forEach { (key: String?, value: String?) -> result.add(key, value) }
+        multivaluedMap: MultivaluedMap<String, String>,
+        multivaluedMap1: MultivaluedMap<String, String>
+    ): MultivaluedMap<String, String> {
+        val result: MultivaluedMap<String, String> = MultivaluedMapImpl<String, String>()
+        UserContext.adapterHeaderMap.forEach { (key: String, value: String) -> result.add(key, value) }
         return result
     }
 }
