0e935ea8c3f5690e93e2fa1d9ec4a7191124a257
Andreas Mautsch <amautsch>
2026-02-27 14:19:19 +0100

    update

--- src/main/kotlin/org/goafabric/personservice/controller/PersonConsumer.kt
+++ src/main/kotlin/org/goafabric/personservice/controller/PersonConsumer.kt
@@ -1,28 +1,23 @@
 package org.goafabric.personservice.controller
 
-import io.opentelemetry.api.trace.Span
-import io.opentelemetry.context.Context
 import jakarta.enterprise.context.ApplicationScoped
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.common.header.Headers
 import org.eclipse.microprofile.reactive.messaging.Incoming
 import org.goafabric.personservice.controller.dto.Person
-import org.goafabric.personservice.extensions.UserContext
-import org.goafabric.personservice.extensions.UserContext.setContext
+import org.goafabric.personservice.extensions.KafkaListener
+import org.goafabric.personservice.extensions.MyKafkaInterceptor
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.slf4j.MDC
-import java.nio.charset.StandardCharsets
 
 @ApplicationScoped
 class PersonConsumer {
     private val log: Logger = LoggerFactory.getLogger(this.javaClass)
 
     @Incoming("person-in")
+    @KafkaListener
     fun listen(consumerRecord: ConsumerRecord<String, Person>)  {
-        setContext(consumerRecord.headers())
-        val person = consumerRecord.value()
-        log.info("loopback person: $person")
+        val operation = MyKafkaInterceptor.getOperation(consumerRecord.headers())
+        log.info("loopback person: " + consumerRecord.value())
     }
 
     /*
@@ -35,21 +30,4 @@ class PersonConsumer {
         log.info("loopback address: " + address.toString())
     }
     */
-
-    private fun setContext(headers: Headers) {
-        setContext(
-            getValue(headers, "X-TenantId"), getValue(headers, "X-OrganizationId"),
-            getValue(headers, "X-Auth-Request-Preferred-Username"), null
-        )
-        configureLogsAndTracing()
-    }
-
-    private fun configureLogsAndTracing() {
-        MDC.put("tenantId", UserContext.tenantId)
-        Span.fromContext(Context.current()).setAttribute("tenant.id", UserContext.tenantId)
-    }
-
-    private fun getValue(headers: Headers, key: String): String {
-        return String(headers.lastHeader(key).value(), StandardCharsets.UTF_8)
-    }
 }
