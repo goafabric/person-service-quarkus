0149864ba259cb807765a6fec79bff161e2bc005
Andreas Mautsch <amautsch>
2026-03-07 10:12:21 +0100

    chang

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
 
 		testImplementation("org.assertj:assertj-core:3.27.7")
 		testImplementation("com.tngtech.archunit:archunit-junit5:1.4.1")
+		testImplementation("org.mockito.kotlin:mockito-kotlin:6.2.3")
 	}
 
 	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.32.2"))
