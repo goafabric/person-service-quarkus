a2219c23d8cb0f4ccb472e74aa37a4b3f336d482
Andreas Mautsch <amautsch>
2026-03-20 10:02:24 +0100

    secrets

--- src/main/kotlin/org/goafabric/personservice/extensions/ConfigTreeSourceFactory.kt
+++ src/main/kotlin/org/goafabric/personservice/extensions/ConfigTreeSourceFactory.kt
@@ -1,9 +1,11 @@
 package org.goafabric.personservice.extensions
 
+import io.quarkus.runtime.annotations.StaticInitSafe
 import io.smallrye.config.ConfigSourceContext
 import io.smallrye.config.ConfigSourceFactory
 import org.eclipse.microprofile.config.spi.ConfigSource
 import java.io.File
+import java.nio.file.Files
 import java.util.*
 
 //https://quarkus.io/guides/config-extending-support#custom-config-source
@@ -18,4 +20,31 @@ class ConfigTreeSourceFactory : ConfigSourceFactory {
     }
 
     override fun getPriority(): OptionalInt = OptionalInt.of(290)
+
+    @StaticInitSafe
+    class ConfigTreeSource(private val root: File) : ConfigSource {
+
+        private val properties: Map<String, String> = load()
+
+        private fun load(): Map<String, String> {
+            val map = mutableMapOf<String, String>()
+
+            root.walkTopDown()
+                .filter { it.isFile }
+                .forEach { file ->
+                    val relative = root.toPath().relativize(file.toPath()).toString()
+                    val key = relative.replace(File.separatorChar, '.').replace(Regex("\\.+"), ".")
+                    val value = Files.readString(file.toPath()).trim()
+                    map[key] = value
+                }
+
+            return map
+        }
+
+        override fun getProperties(): MutableMap<String, String> = properties.toMutableMap()
+        override fun getPropertyNames(): MutableSet<String> = properties.keys.toMutableSet()
+        override fun getValue(propertyName: String?): String? = properties[propertyName]
+        override fun getName(): String = "configtree:${root.absolutePath}"
+        override fun getOrdinal(): Int = 350
+    }
 }
