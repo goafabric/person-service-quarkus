77f1cd8f829732d91db20d283c23a6c3eb79ac05
Andreas Mautsch <amautsch>
2026-05-21 08:46:52 +0200

    chang

--- src/main/kotlin/org/goafabric/personservice/persistence/extensions/TenantResolver.kt
+++ src/main/kotlin/org/goafabric/personservice/persistence/extensions/TenantResolver.kt
@@ -1,10 +1,11 @@
 package org.goafabric.personservice.persistence.extensions
 
 import io.quarkus.hibernate.orm.PersistenceUnitExtension
-import jakarta.enterprise.context.ApplicationScoped
+import io.quarkus.runtime.StartupEvent
+import jakarta.annotation.Priority
 import jakarta.enterprise.context.RequestScoped
+import jakarta.enterprise.event.Observes
 import jakarta.enterprise.inject.spi.CDI
-import jakarta.inject.Inject
 import org.eclipse.microprofile.config.ConfigProvider
 import org.eclipse.microprofile.config.inject.ConfigProperty
 import org.flywaydb.core.Flyway
@@ -15,9 +16,6 @@ import java.util.function.Consumer
 @PersistenceUnitExtension
 @RequestScoped
 class TenantResolver: io.quarkus.hibernate.orm.runtime.tenant.TenantResolver {
-    @Inject
-    internal var flywayConfig: FlywayConfig? = null
-
     @ConfigProperty(name = "multi-tenancy.schema-prefix")
     var schemaPrefix: String? = null
 
@@ -29,27 +27,24 @@ class TenantResolver: io.quarkus.hibernate.orm.runtime.tenant.TenantResolver {
         return schemaPrefix + tenantId
     }
 
-    @ApplicationScoped
-    internal class FlywayConfig() {
-        init {
-            if (ConfigProvider.getConfig().getValue("database.provisioning.goals", String::class.java).contains("-migrate")) {
-                val flyway = CDI.current().select(Flyway::class.java).get()
-                val schemas = ConfigProvider.getConfig().getValue("multi-tenancy.tenants", String::class.java)
-                val schemaPrefix =
-                    ConfigProvider.getConfig().getValue("multi-tenancy.schema-prefix", String::class.java)
-                listOf(*schemas.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray())
-                    .forEach(
-                        Consumer { schema: String? ->
-                            Flyway.configure()
-                                .configuration(flyway.configuration)
-                                .schemas(schemaPrefix + schema)
-                                .defaultSchema(schemaPrefix + schema)
-                                .placeholders(Map.of<String?, String?>("tenantId", schema))
-                                .load()
-                                .migrate()
-                        }
-                    )
-            }
+    fun onStart(@Observes @Priority(1) ev: StartupEvent) {
+        if (ConfigProvider.getConfig().getValue("database.provisioning.goals", String::class.java).contains("-migrate")) {
+            val flyway = CDI.current().select(Flyway::class.java).get()
+            val schemas = ConfigProvider.getConfig().getValue("multi-tenancy.tenants", String::class.java)
+            val schemaPrefix =
+                ConfigProvider.getConfig().getValue("multi-tenancy.schema-prefix", String::class.java)
+            listOf(*schemas.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray())
+                .forEach(
+                    Consumer { schema: String? ->
+                        Flyway.configure()
+                            .configuration(flyway.configuration)
+                            .schemas(schemaPrefix + schema)
+                            .defaultSchema(schemaPrefix + schema)
+                            .placeholders(Map.of<String?, String?>("tenantId", schema))
+                            .load()
+                            .migrate()
+                    }
+                )
         }
     }
 }
