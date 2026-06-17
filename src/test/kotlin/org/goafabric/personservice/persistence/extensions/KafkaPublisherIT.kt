1cea414326375699988f8f3256c3f163b78dd0d7
Andreas Mautsch <amautsch>
2026-06-17 15:21:56 +0200

    chang

--- src/test/kotlin/org/goafabric/personservice/persistence/extensions/KafkaPublisherIT.kt
+++ src/test/kotlin/org/goafabric/personservice/persistence/extensions/KafkaPublisherIT.kt
@@ -11,22 +11,10 @@ import org.goafabric.personservice.logic.PersonLogic
 import org.junit.jupiter.api.Assumptions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ConditionEvaluationResult
-import org.junit.jupiter.api.extension.ExecutionCondition
-import org.junit.jupiter.api.extension.ExtendWith
-import org.junit.jupiter.api.extension.ExtensionContext
 import org.testcontainers.DockerClientFactory
 import java.util.concurrent.TimeUnit
 
-
-class DockerAvailableCondition : ExecutionCondition {
-    override fun evaluateExecutionCondition(context: ExtensionContext): ConditionEvaluationResult =
-        if (DockerClientFactory.instance().isDockerAvailable)
-            ConditionEvaluationResult.enabled("Docker is av") else ConditionEvaluationResult.disabled("Docker n/a")
-}
-
 @QuarkusTest
-@ExtendWith(DockerAvailableCondition::class)
 class KafkaPublisherIT {
     @Inject
     lateinit var personLogic: PersonLogic
@@ -36,6 +24,13 @@ class KafkaPublisherIT {
 
     @Inject
     lateinit var objectMapper: ObjectMapper
+
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun checkDocker() =
+            Assumptions.assumeTrue(DockerClientFactory.instance().isDockerAvailable, "Docker is not running, skipping test")
+    }
 
     @Test
     fun save() {
