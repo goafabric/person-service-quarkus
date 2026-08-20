43f240dd3d37b66ae733f69adfb12171d923c031
Andreas Mautsch <amautsch@gmx.de>
2026-08-20 13:01:40 +0000

    Update all-updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ val dockerRegistry = "goafabric"
 plugins {
 	java
 	jacoco
-	id("io.quarkus") version "3.38.2"
+	id("io.quarkus") version "3.38.3"
 	id("net.researchgate.release") version "3.1.0"
 	id("org.sonarqube") version "7.4.0.8496"
 
@@ -38,8 +38,8 @@ dependencies {
 		testImplementation("org.mockito.kotlin:mockito-kotlin:6.3.0")
 	}
 
-	kapt(enforcedPlatform("io.quarkus:quarkus-bom:3.38.2"))
-	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.38.2"))
+	kapt(enforcedPlatform("io.quarkus:quarkus-bom:3.39.0"))
+	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.39.0"))
 }
 dependencies {
 	//web
