ec40e12f545d997c5c508b62531d693db6460829
Andreas Mautsch <amautsch>
2026-08-21 09:03:58 +0200

    rest fixes

--- src/main/kotlin/org/goafabric/personservice/extensions/HttpInterceptor.kt
+++ src/main/kotlin/org/goafabric/personservice/extensions/HttpInterceptor.kt
@@ -13,7 +13,6 @@ import jakarta.ws.rs.container.ContainerRequestFilter
 import jakarta.ws.rs.container.ContainerResponseContext
 import jakarta.ws.rs.container.ContainerResponseFilter
 import jakarta.ws.rs.ext.Provider
-import org.jboss.resteasy.reactive.server.jaxrs.ContainerRequestContextImpl
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
@@ -28,9 +27,7 @@ class HttpInterceptor : ContainerRequestFilter, ContainerResponseFilter, ToolFil
     override fun filter(request: ContainerRequestContext) {
         UserContext.setContext(request)
         configureLogsAndTracing()
-        if (request is ContainerRequestContextImpl && !request.isPreMatch) {
-            log.info("{} {} http call for user {} ", request.method, request.uriInfo.path, UserContext.userName)
-        }
+        log.info("{} {} http call for user {} ", request.method, request.uriInfo.path, UserContext.userName)
     }
 
     @Throws(IOException::class)
