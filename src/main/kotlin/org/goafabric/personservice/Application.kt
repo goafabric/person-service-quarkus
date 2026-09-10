81cde8d27efd30a6846a29056e7cfa3c4ce97bfa
Andreas Mautsch <amautsch>
2026-09-10 07:41:14 +0200

    jvm image added

--- src/main/kotlin/org/goafabric/personservice/Application.kt
+++ src/main/kotlin/org/goafabric/personservice/Application.kt
@@ -1,10 +1,21 @@
 package org.goafabric.personservice
 
 import io.quarkus.runtime.Quarkus
+import io.quarkus.runtime.StartupEvent
 import io.quarkus.runtime.annotations.QuarkusMain
+import jakarta.enterprise.context.ApplicationScoped
+import jakarta.enterprise.event.Observes
+import org.jboss.logging.Logger
 
+@ApplicationScoped
 @QuarkusMain
-class Application
+class Application {
+    private val log = Logger.getLogger(Application::class.java)
+
+    fun onStart(@Observes event: StartupEvent) {
+        log.info("Running on Java ${System.getProperty("java.version")} " + "(${System.getProperty("java.vendor")}, ${System.getProperty("java.vm.name")})")
+    }
+}
 
 fun main(args: Array<String>) {
     Quarkus.run(*args)
